953e3d58c5792d889ac42a24cdff1492e93fefba
Jaqueline Kirini <bfatskih@gmail.com>
2020-10-22 08:46:02 -0300

    Initial dist subtree commit

---
+++ precache-manifest.a0fc57bb71fd8637fdc30a0024ac4d1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e569e222299a3f7850ed9c2da07da64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc37c0d727f2e9b6f1d6",
+    "url": "/static/css/2.44309f2b.chunk.css"
+  },
+  {
+    "revision": "9f70847c1693f0157e2d",
+    "url": "/static/css/main.9ec65107.chunk.css"
+  },
+  {
+    "revision": "dc37c0d727f2e9b6f1d6",
+    "url": "/static/js/2.a5e52a25.chunk.js"
+  },
+  {
+    "revision": "0fc61ce0ee2d491f41ee7634c63d9b90",
+    "url": "/static/js/2.a5e52a25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f70847c1693f0157e2d",
+    "url": "/static/js/main.7c3a96ba.chunk.js"
+  },
+  {
+    "revision": "f088f1ed8555d0b51150",
+    "url": "/static/js/runtime-main.085145de.js"
+  }
+]);
